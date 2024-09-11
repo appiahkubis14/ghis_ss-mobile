520ce4acedae33ac88af6242eb341053126357d3
appiahkubis14 <appiahkubis14@gmail.com>
2024-09-11 07:51:59 +0100

    change-1

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,3 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class name extends StatefulWidget {
-  const name({super.key});
-
-  @override
-  State<name> createState() => _nameState();
-}
-
-class _nameState extends State<name> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
