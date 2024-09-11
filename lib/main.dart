004a5579af2399e93d23ef85de9b81f81ebec2f7
appiahkubis14 <appiahkubis14@gmail.com>
2024-09-11 07:24:43 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
